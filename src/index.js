2ec03554476349b4bfeced9259d0da9d5eeaa861
Ben Smus <bensmus@gmail.com>
2023-03-21 15:28:42 -0700

    added lodash, u can use any node module with webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import foolaloola from './foo'
-import {min, max} from './bar'
+import {min, max, partitionPivotFirst} from './bar'
 console.log(foolaloola())
 console.log(min(1, 2))
 console.log(max(1, 2))
+console.log(partitionPivotFirst([5, 9, 2, -1, 2, 2, 2]))
